bb0248e4d07f9f7fbaee4b2f8e0005e8b1d1ca4e
Ed Baker <edwbaker@gmail.com>
2017-08-17 17:13:15 +0100

    Default is remote.

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,6 +1,6 @@
 # Seting remote to FALSE allows local operation
 .onLoad <- function(libname, pkgname){
-  remote <- FALSE
+  remote <- TRUE
   if (remote==TRUE){
     c <- bioacoustica::bioacoustica.authenticate(Sys.getenv("BAUSR"), Sys.getenv("BAPWD"))
     generateProlog(c)
